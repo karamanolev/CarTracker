2004c837bd8d34ff1c0f2ab8830602e6753da6c0
Ivailo Karamanolev <ivailo@karamanolev.com>
2017-12-27 00:27:48 +0200

    Fix scrape link admin

--- mobile_bg/admin.py
+++ mobile_bg/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from mobile_bg.models import MobileBgAd, MobileBgScrapeLink
 
+
+class MobileBgScrapeLinkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slink')
+
+
 admin.site.register(MobileBgAd)
-admin.site.register(MobileBgScrapeLink)
+admin.site.register(MobileBgScrapeLink, MobileBgScrapeLinkAdmin)
